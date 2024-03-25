8661568453cc4ac022a16a04e40596e3f61b5a3c
Luojun <1771096201@qq.com>
2024-03-25 22:29:23 +0800

    push commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,6 +12,6 @@ public class GitTest {
         System.out.println("4");
         System.out.println("master test");
         System.out.println("hot-fix test!");
-        System.out.println("push test!");
+
     }
 }
