950a45510936f093bcf4e1ed0576e7144bd0afd6
Luojun <1771096201@qq.com>
2024-03-25 22:04:34 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
         System.out.println("hello git！");
         System.out.println("2");
         System.out.println("3");
-        System.out.println("4");
+        System.out.println("hot-fix test!");
     }
 }
