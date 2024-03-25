309e3a062c4de91f655d44b93d4f52324c75e653
Luojun <1771096201@qq.com>
2024-03-25 21:57:21 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git！");
         System.out.println("2");
+        System.out.println("3");
     }
 }
