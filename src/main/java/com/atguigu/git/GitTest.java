b0df0793af2fbd901cad428c18da63a22113ea0e
fancyjun <121794665+fancyjun@users.noreply.github.com>
2024-03-26 10:08:44 +0800

    commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("push test");
 System.out.println("666!");
+        System.out.println("666");
 
     }
 }
